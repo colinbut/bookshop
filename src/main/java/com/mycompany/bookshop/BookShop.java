7de5a584f016bc9d178f2c89498125f7c9f95711
Colin But <colinbut@users.noreply.github.com>
2017-11-11 23:40:11 +0000

    refactoring the logic cleaning up code and test

--- src/main/java/com/mycompany/bookshop/BookShop.java
+++ src/main/java/com/mycompany/bookshop/BookShop.java
@@ -7,10 +7,14 @@ package com.mycompany.bookshop;
 
 public class BookShop {
 
+    private static final int BOOK_YEAR_OVER_2010 = 2010;
+    private static final int BOOK_YEAR_OVER_2010_DISCOUNT = 10; // in %
+
     public double buyBook(Book book) {
-        if (book.getYear() >= 2010) {
-            return book.getPrice() - (book.getPrice() * 0.10);
+        double bookPrice = book.getPrice();
+        if (book.getYear() >= BOOK_YEAR_OVER_2010) {
+            return bookPrice - (bookPrice * ((double) BOOK_YEAR_OVER_2010_DISCOUNT / 100));
         }
-        return book.getPrice();
+        return bookPrice;
     }
 }
--- src/test/java/com/mycompany/bookshop/BookShopUTest.java
+++ src/test/java/com/mycompany/bookshop/BookShopUTest.java
@@ -5,21 +5,27 @@
  */
 package com.mycompany.bookshop;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BookShopUTest {
 
+    private BookShop bookShop;
+
+    @Before
+    public void setUp(){
+        bookShop = new BookShop();
+    }
+
     @Test
     public void givenBookWithYearGreaterThan2010_whenBuyBook_thenBuyBookWith10PercentDiscount() {
         double expectedCostOfBook = 11.826;
         Book book = new Book("The Terrible Privacy of Maxwell Sim ", 13.14, 2010);
 
-        BookShop bookShop = new BookShop();
         double costOfBook = bookShop.buyBook(book);
 
         assertThat(costOfBook).isEqualTo(expectedCostOfBook);
-
     }
 }
