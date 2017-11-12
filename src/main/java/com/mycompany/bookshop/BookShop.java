ba57b6fd77386acb7bc2f7eb1f017ca48e2c3b0c
Colin But <colinbut@users.noreply.github.com>
2017-11-12 19:25:57 +0000

    5% discount logic on books worth total

--- src/main/java/com/mycompany/bookshop/BookShop.java
+++ src/main/java/com/mycompany/bookshop/BookShop.java
@@ -5,10 +5,14 @@
  */
 package com.mycompany.bookshop;
 
+import java.util.List;
+
 public class BookShop {
 
     private static final int BOOK_YEAR_OVER_2010 = 2010;
     private static final int BOOK_YEAR_OVER_2010_DISCOUNT = 10; // in %
+    private static final int TOTAL_BOOKS_WORTH_MORE_THAN_30_POUNDS_DISCOUNT = 5;
+    private static final double TOTAL_BOOKS_WORTH_MORE_THAN_30_POUNDS = 30.00;
 
     public double buyBook(Book book) {
         double bookPrice = book.getPrice();
@@ -16,5 +20,18 @@ public class BookShop {
             return bookPrice - (bookPrice * ((double) BOOK_YEAR_OVER_2010_DISCOUNT / 100));
         }
         return bookPrice;
+    }
+
+    public double buyBooks(List<Book> books) {
+        double totalCost = 0.0;
+        for (Book book : books) {
+            totalCost += buyBook(book);
+        }
+
+        if (totalCost > TOTAL_BOOKS_WORTH_MORE_THAN_30_POUNDS) {
+            return totalCost - (totalCost * ((double) TOTAL_BOOKS_WORTH_MORE_THAN_30_POUNDS_DISCOUNT / 100));
+        }
+
+        return totalCost;
     }
 }
--- src/test/java/com/mycompany/bookshop/BookShopUTest.java
+++ src/test/java/com/mycompany/bookshop/BookShopUTest.java
@@ -8,6 +8,8 @@ package com.mycompany.bookshop;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BookShopUTest {
@@ -27,5 +29,28 @@ public class BookShopUTest {
         double costOfBook = bookShop.buyBook(book);
 
         assertThat(costOfBook).isEqualTo(expectedCostOfBook);
+    }
+
+    @Test
+    public void givenBookWithYearNotGreaterThan2010_whenBuyBook_thenBuyBookWithOriginalPrice() {
+        double expectedCostOfBook = 15.16;
+        Book book = new Book("The Terrible Privacy of Maxwell Sim ", 15.16, 1851);
+
+        double costOfBook = bookShop.buyBook(book);
+
+        assertThat(costOfBook).isEqualTo(expectedCostOfBook);
+    }
+
+    @Test
+    public void givenTotalAmountOfBookWorthOver30Pounds_whenBuyBooks_thenGet5PercentDiscountOnTheTotal() {
+        double expectedCostOfBook = 35.27;
+
+        Book book1 = new Book("Still Life With Woodpecker", 11.05, 1980);
+        Book book2 = new Book("Three Men in a Boat", 12.87, 1889);
+        Book book3 = new Book("Great Expectations", 13.21, 1861);
+
+        double costOfBooks = bookShop.buyBooks(Arrays.asList(book1, book2, book3));
+
+        assertThat(costOfBooks).isEqualTo(expectedCostOfBook);
     }
 }
